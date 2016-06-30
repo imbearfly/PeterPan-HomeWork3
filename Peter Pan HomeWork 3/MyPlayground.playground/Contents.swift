b8808b077815a01d95c2c4c5f56a81c55e47421e
Fly <imbearfly@efgpdigiwinde-macbook-air.local>
2016-06-30 16:06:46 +0800

    作業三完成

--- Peter Pan HomeWork 3/MyPlayground.playground/Contents.swift
+++ Peter Pan HomeWork 3/MyPlayground.playground/Contents.swift
@@ -3,7 +3,7 @@
 import UIKit
 
 
-// 練習一
+// 算全部格子的總和
 var sum1 = 0
 for i1 in 1...7{
     for v1 in 1...7{
@@ -12,7 +12,7 @@ for i1 in 1...7{
 }
 sum1
 
-// 練習二
+// 算奇數行的總和
 var sum2 = 0
 for i2 in 1...7 {
     for v2 in 1...7{
@@ -23,7 +23,7 @@ for i2 in 1...7 {
 }
 sum2
 
-//練習三
+//奇數偶數func
 var sum3 = 0
 for i3 in 1...7{
     for v3 in 1...7{
@@ -37,21 +37,87 @@ for i3 in 1...7{
 }
 sum3
 
+//func 最大值 最小值 倍數
+var sum4 = 0
+func minMaxTotal(min:Int,max:Int,range:Int)->Int{
+    for i in min...max{
+        if i % range == 0{
+            sum4 += i
+        }
+    }
+    return sum4
+}
+
+var sum5 = 0
+func minMaxTotal2(min:Int,max:Int,range:Int)->Int{
+    var numNow = min
+    while numNow < max{
+        if numNow % range == 0 {
+            sum5 += numNow
+            numNow += range
+        }else{
+            numNow += 1
+        }
+    }
+    return sum5
+}
+
+minMaxTotal(min: 60, max: 1000, range: 6)
+minMaxTotal2(min: 2, max: 1000, range: 8)
+
+//func 奇數行總和
+var sum6 = 0
+func oddRowsSum(rows:Int,cols:Int )->Int{
+    for i in 0...rows{
+        if i % 2 == 1{
+            for j in 0...cols {
+                sum6 += (i * j)
+            }
+        }
+    }
+    return sum6
+}
+
+oddRowsSum(rows: 5 ,cols: 11)
+
+
+//func 列數>=行數總和
+var sum7 = 0
+func countSum(rows:Int,cols:Int)->Int{
+    for i in 0...rows{
+        for j in 0...cols{
+            if i >= j {
+                sum7 += (i * j)
+            }
+        }
+    }
+    return sum7
+}
+countSum(rows: 10, cols: 50)
+
+//華氏轉攝氏
+var celsius = 0.0
+func fahrenheitToCelsius(f:Double)-> Double{
+    celsius = (f-32)*5/9
+    return celsius
+}
+fahrenheitToCelsius(f: 200)
+
 //練習最後一題
 
 func oddOrEvens(nums:[Int])->[(title:String,values:[Int])]{
     var odds = [Int]()
     var evens = [Int]()
     for i in nums{
-        if(nums[i-1]%2 == 1){
-            odds.append(nums[i-1])
+        if(i % 2 == 1){
+            odds.append(i)
         }else{
-            evens.append(nums[i-1])
+            evens.append(i)
         }
     }
     return [("Odd numbers: ",odds),("Even numbers: ",evens)]
 }
-var nummm = [1,2,3,4,5,6,7,8,9,10]
+var nummm = [1,2,3,4,5,6,7,8,9,10,3,5,8,11,23,54,16]
 var times = oddOrEvens(nums: nummm).count
 for i in 0...times-1{
   print("\(oddOrEvens(nums: nummm)[i].title) \(oddOrEvens(nums: nummm)[i].values)")
